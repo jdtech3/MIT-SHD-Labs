118c2c0946b7f609cb2683964424b1f3058ade01
Joe Dai <dm.joedai@gmail.com>
2024-06-10 18:24:17 +0000

    Lab2 Part1

--- SHD-CacheAttackLab/Part1-Timing/main.c
+++ SHD-CacheAttackLab/Part1-Timing/main.c
@@ -8,10 +8,10 @@
 
 #define LINE_SIZE 64
 // [1.2] TODO: Uncomment the following lines and fill in the correct size
-//#define L1_SIZE TODO
-//#define L2_SIZE TODO
-//#define L3_SIZE TODO
-//#define BUFF_SIZE TODO
+#define L1_SIZE 49152
+#define L2_SIZE 2097152
+#define L3_SIZE 23592960
+#define BUFF_SIZE 4*L3_SIZE
  
 int main (int ac, char **av) {
 
@@ -37,7 +37,7 @@ int main (int ac, char **av) {
 
     // [1.2] TODO: Uncomment the following line to allocate a buffer of a size
     // of your chosing. This will help you measure the latencies at L2 and L3.
-    //volatile uint8_t *eviction_buffer = (uint8_t *)malloc(BUFF_SIZE);
+    volatile uint8_t *eviction_buffer = (uint8_t *)malloc(BUFF_SIZE);
 
     // Example: Measure L1 access latency, store results in l1_latency array
     for (int i=0; i<SAMPLES; i++){
@@ -53,30 +53,63 @@ int main (int ac, char **av) {
     // [1.2] TODO: Measure DRAM Latency, store results in dram_latency array
     // ======
     //
+    
+    for (int i=0; i<SAMPLES; i++){
+        for (int i = 0; i < LINE_SIZE; i++) clflush((void*)(target_buffer + i));
+        dram_latency[i] = measure_one_block_access_time((uint64_t)target_buffer);
+    }
 
     // ======
     // [1.2] TODO: Measure L2 Latency, store results in l2_latency array
     // ======
     //
+    
+    for (int i=0; i<SAMPLES; i++){
+        // pull into cache
+        // for (int i = 0; i < LINE_SIZE; i++) clflush((void*)(target_buffer + i));
+        for (int i = 0; i < LINE_SIZE; i++) tmp = target_buffer[i];
+        lfence();
+
+        // evict everything from L1
+        for (int i = 0; i < 4*L1_SIZE; i++) tmp = eviction_buffer[i];
+        lfence();
+
+        // measure
+        l2_latency[i] = measure_one_block_access_time((uint64_t)target_buffer);
+    }
 
     // ======
     // [1.2] TODO: Measure L3 Latency, store results in l3_latency array
     // ======
     //
+    for (int i=0; i<SAMPLES; i++){
+        // pull into cache
+        // for (int i = 0; i < LINE_SIZE; i++) clflush((void*)(target_buffer + i));
+        for (int i = 0; i < LINE_SIZE; i++) tmp = target_buffer[i];
+        lfence();
 
+        // evict everything from L2
+        for (int i = 0; i < 4*L2_SIZE; i++) tmp = eviction_buffer[i];
+        lfence();
+
+        // measure
+        l3_latency[i] = measure_one_block_access_time((uint64_t)target_buffer);
+    }
 
     // Print the results to the screen
     // When compile to main and used by `make run`,
     // it uses print_results_plaintext
     // When compile to main-visual and used by `run.py`,
     // it uses print_results_for_visualization
+    // for (int i = 0; i < SAMPLES; i++) printf("%ld ", l2_latency[i]);
+    // printf("\n");
     PRINT_FUNC(dram_latency, l1_latency, l2_latency, l3_latency);
 
     free((uint8_t *)target_buffer);
 
     // [1.2] TODO: Uncomment this line once you uncomment the eviction_buffer
     //             creation line
-    //free((uint8_t *)eviction_buffer);
+    free((uint8_t *)eviction_buffer);
     return 0;
 }
 
